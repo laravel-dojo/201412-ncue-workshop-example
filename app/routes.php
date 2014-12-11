a3d8da67da75bad3a196c1386b0f5b62479bac24
Shengyou Fan <shengyoufan@gmail.com>
2014-12-12 01:47:02 +0800

    [Route機制] 設定應用程式 Route
    在這個步驟裡，我們依照專案需求設定 Blog 所需要用到的 Route 規則

--- app/routes.php
+++ app/routes.php
@@ -11,133 +11,64 @@
 |
 */
 
+Route::pattern('id', '[0-9]+');
+
 Route::get('/', function()
 {
-	return View::make('hello');
+	return 'Home Page';
 });
 
-Route::get('new-post-1', function()
+Route::get('about', function()
 {
-	$post = new Post;
-    $post->title = 'My new post title';
-    $post->save();
-    
-    echo '<pre>';
-    dd($post);
+	return 'About Page';
 });
 
-Route::get('new-post-2', function()
+Route::get('categories', function()
 {
-	$post = Post::create([
-        'title'   => 'My new post title',
-        'content' => 'My new post content',
-    ]);
-    
-    echo '<pre>';
-    dd($post);
+	return 'Categories List';
 });
 
-Route::get('delete-post', function()
+Route::get('categories/{id}', function($id)
 {
-	$post = Post::find(1);
-    $post->delete();
-    
-    echo '<pre>';
-    dd($post);
+	return 'Show Category: '.$id;
 });
 
-Route::get('destroy-post', function()
+Route::get('categories/create', function()
 {
-	Post::destroy(2);
-    Post::destroy([3, 4, 5]);
-    Post::destroy(6, 7, 8);
-
-    dd('done');
+	return 'Create a new Category';
 });
 
-Route::get('update-post-1', function()
+Route::get('categories/{id}/edit', function($id)
 {
-	$post = Post::find(9);
-    $post->title = 'Update my post title';
-    $post->save();
-    
-    echo '<pre>';
-    dd($post);
+	return 'Edit Category: '.$id;
 });
 
-Route::get('update-post-2', function()
+Route::get('posts', function()
 {
-	$post = Post::find(10);
-    $post->update([
-        'title'   => 'My update post title',
-        'content' => 'My update post content',
-    ]);
-    
-    echo '<pre>';
-    dd($post);
+	return 'Posts List';
 });
 
-Route::get('find', function()
+Route::get('posts/{id}', function($id)
 {
-    echo '<pre>';
-    
-	$category_posts = Post::all();
-    var_dump($category_posts);
-    
-    $first_post_of_category = Post::find(11);
-    var_dump($first_post_of_category);
-    
-    $category_posts = Post::where('category_id', '=', 1)->orderBy('created_at', 'desc')->get();
-    var_dump($category_posts);
-    
-    $first_post_of_category = Post::where('category_id', '=', 1)->orderBy('created_at', 'desc')->first();
-    var_dump($first_post_of_category);
-    
-    echo '</pre>';
- 
-    dd('done');
+	return 'Show Post: '.$id;
 });
 
-Route::get('collection', function()
+Route::get('posts/create', function()
 {
-    $category_posts = Post::where('category_id', '=', 1)->orderBy('created_at', 'desc')->get();
-    foreach($category_posts as $post)
-    {
-        echo $post->id.': '.$post->title.'<br>';
-    }
- 
-    dd();
+	return 'Create a new Post';
+});
+
+Route::get('posts/{id}/edit', function($id)
+{
+	return 'Edit Post: '.$id;
 });
 
 Route::get('random', function()
 {
-    echo '<pre>';
-    
-    $random_post = Post::where('category_id', '=', 1)->orderBy('created_at', 'desc')->get()->random();
-    var_dump($random_post);
-    
-    $random_posts = Post::where('category_id', '=', 1)->orderBy('created_at', 'desc')->get()->random(3);
-    var_dump($random_posts);
- 
-    echo '</pre>';
-    
-    dd('done');
+	return 'Random Post!';
 });
 
-Route::get('relationship', function()
+Route::get('login', function()
 {
-    $post = Post::find(1);
-    
-    echo $post->category->name.'<br>';
-    foreach($post->comments as $comment)
-    {
-        echo $comment->id.': '.$comment->content.'<br>';
-    }
-    
-    echo '<pre>';
-    var_dump($post->category);
-    var_dump($post->comments);
-    echo '</pre>';
-    
-    dd('done');
+	return 'Login Page';
 });
