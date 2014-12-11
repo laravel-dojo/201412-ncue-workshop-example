62f173b0fa68f978ee0a7fc22ac84a8fe5a619ef
Shengyou Fan <shengyoufan@gmail.com>
2014-12-11 15:44:10 +0800

    [專案啟動與設定] 安裝套件
    在這個步驟裡，我們學習如何設定 composer.json、app.php 來安裝三個開發用的套件，並把 ide-helper 產生的檔案忽略掉

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -15,4 +15,21 @@ return array(
 
 	'debug' => true,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
+
+    'providers' => append_config(array(
+        /* Additional Providers */
+        'Way\Generators\GeneratorsServiceProvider',
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+    )),
+
 );
