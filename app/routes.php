a32d609d0a6fec1babecf43235aad168978e5c91
Shengyou Fan <shengyoufan@gmail.com>
2014-12-11 19:09:59 +0800

    [使用Eloquent ORM] 示範 ORM 用法
    在這個步驟裡，我們直接在 Route 內使用 dd() 來示範 ORM 的用法

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,111 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('new-post-1', function()
+{
+	$post = new Post;
+    $post->title = 'My new post title';
+    $post->save();
+    
+    echo '<pre>';
+    dd($post);
+});
+
+Route::get('new-post-2', function()
+{
+	$post = Post::create([
+        'title'   => 'My new post title',
+        'content' => 'My new post content',
+    ]);
+    
+    echo '<pre>';
+    dd($post);
+});
+
+Route::get('delete-post', function()
+{
+	$post = Post::find(1);
+    $post->delete();
+    
+    echo '<pre>';
+    dd($post);
+});
+
+Route::get('destroy-post', function()
+{
+	Post::destroy(2);
+    Post::destroy([3, 4, 5]);
+    Post::destroy(6, 7, 8);
+
+    dd('done');
+});
+
+Route::get('update-post-1', function()
+{
+	$post = Post::find(9);
+    $post->title = 'Update my post title';
+    $post->save();
+    
+    echo '<pre>';
+    dd($post);
+});
+
+Route::get('update-post-2', function()
+{
+	$post = Post::find(10);
+    $post->update([
+        'title'   => 'My update post title',
+        'content' => 'My update post content',
+    ]);
+    
+    echo '<pre>';
+    dd($post);
+});
+
+Route::get('find', function()
+{
+    echo '<pre>';
+    
+	$category_posts = Post::all();
+    var_dump($category_posts);
+    
+    $first_post_of_category = Post::find(11);
+    var_dump($first_post_of_category);
+    
+    $category_posts = Post::where('category_id', '=', 1)->orderBy('created_at', 'desc')->get();
+    var_dump($category_posts);
+    
+    $first_post_of_category = Post::where('category_id', '=', 1)->orderBy('created_at', 'desc')->first();
+    var_dump($first_post_of_category);
+    
+    echo '</pre>';
+ 
+    dd('done');
+});
+
+Route::get('collection', function()
+{
+    $category_posts = Post::where('category_id', '=', 1)->orderBy('created_at', 'desc')->get();
+    foreach($category_posts as $post)
+    {
+        echo $post->id.': '.$post->title.'<br>';
+    }
+ 
+    dd();
+});
+
+Route::get('random', function()
+{
+    echo '<pre>';
+    
+    $random_post = Post::where('category_id', '=', 1)->orderBy('created_at', 'desc')->get()->random();
+    var_dump($random_post);
+    
+    $random_posts = Post::where('category_id', '=', 1)->orderBy('created_at', 'desc')->get()->random(3);
+    var_dump($random_posts);
+ 
+    echo '</pre>';
+    
+    dd('done');
+});
