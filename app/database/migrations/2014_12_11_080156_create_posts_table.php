4b770dbfc0aaf0ecb825a4d206aecb3bea12ccff
Shengyou Fan <shengyoufan@gmail.com>
2014-12-11 16:45:53 +0800

    [Schema/Migration操作] 實作 migrations
    在這個步驟裡，我們學習如何用 artisan 指令產生 migration 檔，並在 up/down 函式裡用  Shema Builder 操作資料庫

---
+++ app/database/migrations/2014_12_11_080156_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->integer('category_id')->unsigned()->index();
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
